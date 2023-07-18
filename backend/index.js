dfc3aaf28ba239f7db0dcd8d5425ac50e40b8021
Kevin Pereira <kevinpereira@Eclipse.local>
2023-07-18 23:06:12 +0530

    added login endpoint

--- backend/index.js
+++ backend/index.js
@@ -1,22 +1,34 @@
 const connectToMongo = require("./db");
 const express = require("express");
 
+
+// calling the function in "db.js" whihc establishes connection with our mongoDB database
 connectToMongo();
 
+
+// creating the express app on port 3000
 const app = express();
 const port = 3000;
 
+
+// parses the req body as json if the content-type of the request is set as application/json
+// it's a middle ware that only intercepts if the req content-type is application/json
 app.use(express.json())
 
+
+// the root endpoint that sends a response 
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
-// available rounter
 
+
+// keeping routing in other files to keep index.js clean and simple
 app.use('/api/auth',require('./routes/auth'));
 app.use('/api/notes',require('./routes/notes'));
 
+
+// starting the application by making it listen for requests on port 3000
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -2,6 +2,10 @@ const express = require("express");
 const router = express.Router();
 const User = require("../models/User");
 const { body, validationResult } = require("express-validator");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+
+const JWT_SECRET = "lwkjafld4@3kjfaoiweourowj2392i349u";
 
 //create a user using POST: "/api/auth/createuser". No login required
 router.post(
@@ -27,16 +31,69 @@ router.post(
           .status(400)
           .json({ error: "Sorry a user with this email already exists" });
       }
+      const salt = await bcrypt.genSalt(10);
+      const secPassword = await bcrypt.hash(req.body.password, salt);
+
       user = await User.create({
         name: req.body.name,
         email: req.body.email,
-        password: req.body.password,
+        password: secPassword,
       });
-      res.json(user);
+      const data = {
+        user: {
+          id: user.id,
+        },
+      };
+      const authToken = jwt.sign(data, JWT_SECRET);
+      // res.json(user);
+
+      // using ES6 syntax autos to authToken: authToken
+      res.json({ authToken });
     } catch (error) {
       console.log(error.message);
       res.status(500);
       res.send("some error has occured");
+    }
+  }
+);
+
+// authenticate a user using POST: /api/auth/login. no login required
+
+router.post(
+  "/login",
+  [
+    body("email", "enter a valid email").isEmail(), // if email isn't even an email then we don't bother the server
+    body("password", "password cannot be blank").exists(), // checks that password during login must not be blank
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+    try {
+      let user = await User.findOne({ email: req.body.email });
+      if (!user) {
+        return res
+          .status(400)
+          .json({ error: "username/password invalid please try again" });
+      }
+      bcrypt.compare(req.body.password, user.password, (err, result) => {
+        if (result) {
+          const payload = {
+            user: {
+              id: user.id,
+            },
+          };
+          const authToken = jwt.sign(payload, JWT_SECRET);
+          return res.status(200).json({ authToken });
+        } else {
+          return res
+            .status(400)
+            .json({ error: "username/password invalid please try again" });
+        }
+      });
+    } catch (error) {
+      res.status(500).send("some error has occured");
     }
   }
 );
