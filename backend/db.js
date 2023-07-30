0f991dfb13bbd31e74ee2d4cfbf0cbfcdcc0e1a8
Kevin Pereira <kevinpereira2113@gmail.com>
2023-07-30 21:59:31 +0530

    mongo url

--- backend/db.js
+++ backend/db.js
@@ -1,8 +1,7 @@
 const mongoose = require("mongoose");
 require("dotenv").config();
 
-const mongoURI =
-  "mongodb+srv://kevinpereira2113:tBtn2hkp28WubfBU@cluster0.cnwjerc.mongodb.net/iNotebook?retryWrites=true&w=majority";
+const mongoURI = <YOUR MONGO DB TOKEN>;
 const connectToMongo = () => {
   mongoose.connect(mongoURI).then(() => {
     console.log("connected to server successfully");
