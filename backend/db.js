2141723d5e55cb87f7961076e6d46b86176e3738
Kevin Pereira <kevinpereira2113@gmail.com>
2023-07-30 22:00:11 +0530

    mongo uri in db

--- backend/db.js
+++ backend/db.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 require("dotenv").config();
 
-const mongoURI = <YOUR MONGO DB TOKEN>;
+const mongoURI = <YOUR MONGO DB URI>;
 const connectToMongo = () => {
   mongoose.connect(mongoURI).then(() => {
     console.log("connected to server successfully");
